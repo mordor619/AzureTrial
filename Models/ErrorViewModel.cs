cd91c45726b38f73de2c06b7f137f250e7c6052c
unknown <Administrator@KANINI-LTP-459.gisbizdev.gisbiz.chennai.com>
2021-10-08 11:51:44 +0530

    final

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace JwtAuthorisationProj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
